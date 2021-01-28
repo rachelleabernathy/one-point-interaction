949398360bec1b5d6fe1643be79470d6a19e51c4
rachelleabernathy <57782445+rachelleabernathy@users.noreply.github.com>
2021-01-27 20:45:49 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,24 +1,30 @@
-var words = ["a lack of transportation", "a lack of availability", "a lack of funding"];
+var words = ["Approximately 1.6 million Minnesotans have low retail access to grocery stores.", 
+"Minnesota’s total share of residents with low retail access ranks among the ten highest in the U.S.", 
+"Both price and distance create barriers to healthy food access, but price constitutes a more significant barrier.", 
+"An estimated 341,000 Minnesotans face both income and distance barriers to purchasing healthy food.", 
+"Rural residents, low-income residents, senior residents, and residents of color are the most impacted.", 
+"Counties in rural Minnesota have a disproportionate number of food deserts relative to their total population and geographic area."];
 
 var index = 0;
 
 function setup() {
-  createCanvas(630, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
 
-  background('rgb(33, 48, 0)');
+  background('rgb(51, 153, 102)');
 
-  fill(255);
-  textSize(26);
+  fill('rgb(255, 255,230)');
+  textSize(22);
   text(words[index], 50, 200);
   
   textSize(26);
-  text('Minnesota food deserts impact', 50 , 110);
-  text('nearly 50% of residents because of', 50 , 150);
+  text('Minnesota Food Access', 50 , 110);
+  textFont('Oswald');
   textSize(12);
-  text('click mouse', 550 , 380);
+  fill('black');
+  text('click mouse to change facts.', 1250 , 700);
 }
 
 function mousePressed() {
